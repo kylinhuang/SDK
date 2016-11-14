5b44bab19f03c1d933b0a2fa22b60eb141cc0862
kylinhuang <kylinhuang1029@sina.cn>
2016-11-14 16:46:25 +0800

    add so file

--- TencentLiveSDK/app/src/main/java/com/kylin/tencentlivesdk/MainActivity.java
+++ TencentLiveSDK/app/src/main/java/com/kylin/tencentlivesdk/MainActivity.java
@@ -1,13 +1,18 @@
 package com.kylin.tencentlivesdk;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import com.tencent.av.sdk.AVContext;
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // 打印SDK的版本信息
+        Log.i("HelloSDK", "SDK Version = " + AVContext.getVersion());
     }
 }
